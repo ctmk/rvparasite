6da6795f1f67c7c7715cde44e1baaa29bab62f1a
Nobu <nobu@ctmk.amanek.jp>
2014-04-29 21:47:33 +0900

    rvp ver.0.1.0

---
+++ src/rvp/rvp_process.cpp
@@ -0,0 +1,39 @@
+﻿#include "rvp_process.h"
+#include <stdexcept>
+
+namespace rvp {
+namespace process {
+
+	DWORD boot(const TCHAR *filename, TCHAR *commandLineArguments)
+	{
+		STARTUPINFO startupinfo = { 0 };
+		GetStartupInfo(&startupinfo);
+#ifndef RVP_DEBUG
+		startupinfo.wShowWindow = SW_HIDE;
+#endif
+		PROCESS_INFORMATION processinfo = { 0 };
+
+		{
+			const BOOL result = CreateProcess(filename, commandLineArguments, NULL, NULL, TRUE, 0, NULL, NULL, &startupinfo, &processinfo);
+			if (result != TRUE) {
+				throw std::runtime_error("Failed to create process");
+			}
+			CloseHandle(processinfo.hThread);
+		}
+
+		WaitForSingleObject(processinfo.hProcess, INFINITE);
+
+		DWORD exitcode = 0;
+		{
+			const BOOL result = GetExitCodeProcess(processinfo.hProcess, &exitcode);
+			CloseHandle(processinfo.hProcess);
+			if (result != TRUE) {
+				throw std::runtime_error("Failed to get exit code");
+			}
+		}
+
+		return exitcode;
+	}
+
+}
+}
---
+++ src/rvp/rvp_process.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+namespace rvp {
+namespace process {
+
+	/*!
+	 * @param [in] filename
+	 * @param [in] commandLineArguments
+	 * @return exit-code
+	 * @throw std::runtime_error
+	 */
+	DWORD boot(const TCHAR *filename, TCHAR *commandLineArguments = TEXT(""));
+}
+}
